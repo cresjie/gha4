c2889229a4df9b65cffb3593e1c3e4b9ea1829cc
Cres Jie Labasano <cresjie@gmail.com>
2016-09-14 22:53:42 -0700

    updates added htaccess

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,3 +1,12 @@
 $(function(){
-	$('[data-toggle="tooltip"]').tooltip(); 
+	$('[data-toggle="tooltip"]').tooltip();
+	$('[auto-width]').each(function(i,el){
+		$(this).data('input-size', this.size);
+	}).on('keydown', function(){
+		var l = this.value.length;
+		var original = o = $(this).data('input-size');
+			original = o ? o : 20;
+			
+		this.size = l ? l : original;
+	});
 });
