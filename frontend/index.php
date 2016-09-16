9ec162f9057459cb6cd654f70274a78c7155b7d9
Cres Jie Labasano <cresjie@gmail.com>
2016-09-15 23:01:21 -0700

    list all html

---
+++ frontend/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$files = array_diff(scandir(__DIR__),['..','.']);
+?>
+
+<ul>
+	<?php foreach($files as $file): ?>
+		<?php if(!is_dir($file) && pathinfo($file,PATHINFO_EXTENSION) == 'html'): ?>
+			<li>
+				<a href="<?=$file?>"> <?=$file?> </a>
+			</li>
+		<?php endif; ?>
+	<?php endforeach;?>
+</ul>
+
